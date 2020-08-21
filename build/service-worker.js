861327ee9d134061aa9f08372cb6613fc71dec60
TruongTrang <truongtrang.it.spkt@gmail.com>
2020-08-21 20:54:15 +0700

    push build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.26fa45c93cbbb158d01daf23fad1a3d6.js"
+  "/precache-manifest.e07357b747c5229e2fcfc3edfd151c24.js"
 );
 
 self.addEventListener('message', (event) => {
--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,5 +1,5 @@
- export const backend_url = 'http://100.26.249.85:8080/api'
+ export const backend_url = 'http://54.146.60.140/api'
 //export const backend_url = 'http://localhost:3001/api'
 // export const frontend_url= 'http://18.234.208.228:3000'
-export const frontend_url= 'http://localhost:3000'
+export const frontend_url= 'http://54.146.60.140/'
 
