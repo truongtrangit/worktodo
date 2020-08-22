6884060be6e42b47e07a6ae6b66e34a672a4f5c5
TruongTrang <truongtrang.it.spkt@gmail.com>
2020-08-22 09:29:38 +0700

    Updates

---
+++ precache-manifest.f1d2374cc10ff0518411db06e24acd7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87131b2d82e2b65df2bc0549135425ad",
+    "url": "/worktodo/index.html"
+  },
+  {
+    "revision": "eef9c3550be3a9d10c41",
+    "url": "/worktodo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d001642f1ea90e78760b",
+    "url": "/worktodo/static/js/2.96934f21.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/worktodo/static/js/2.96934f21.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eef9c3550be3a9d10c41",
+    "url": "/worktodo/static/js/main.4c8f5944.chunk.js"
+  },
+  {
+    "revision": "8025983f4922493342d5",
+    "url": "/worktodo/static/js/runtime-main.50a82e2b.js"
+  }
+]);
